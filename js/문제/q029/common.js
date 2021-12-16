39d2feb2b532d7eb40b5bca7a7b2c83ffefa1449
zm445 <77592061+zm445@users.noreply.github.com>
2021-12-16 17:30:24 +0900

    Add files via upload

--- js/문제/q029/common.js
+++ js/문제/q029/common.js
@@ -5,15 +5,3 @@ function dw(str){
 function br(){
     document.write("<br>");
 }
-
-function RPS(){
-    if(player === 0 && com === 1){
-        dw("computer win!");
-    } else if(player === 1 && com === 2){
-        dw("computer win!");
-    } else if(player == 2 && com == 0){
-        dw("computer win!");
-    } else {
-        dw("player win!");
-    }
-}
--- js/문제/q029/q029.js
+++ js/문제/q029/q029.js
@@ -1,5 +1,16 @@
 let com = Math.floor(Math.random() * 3);
 let player = prompt("가위(0) 바위(1) 보(2) 입력: ");
 
-RPS();
+if(player == com){
+    dw("가위(0) 바위(1) 보(2) draw <br>player: " + player + "<br>com: " + com);
+}
+else if(player == 0 && com == 1){
+    dw("가위(0) 바위(1) 보(2) computer win! <br>player: " + player + "<br>com: " + com );
+} else if(player == 1 && com == 2){
+    dw("가위(0) 바위(1) 보(2) computer win! <br>player: " + player + "<br>com: " + com );
+} else if(player == 2 && com == 0){
+    dw("가위(0) 바위(1) 보(2) computer win! <br>player: " + player + "<br>com: " + com );
+} else {
+    dw("가위(0) 바위(1) 보(2) player win! <br>player: " + player + "<br>com: " + com);
+}
 
