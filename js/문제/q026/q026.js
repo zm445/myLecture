6abc74318d095c60c82f4aa4133926e3571181a6
zm445 <77592061+zm445@users.noreply.github.com>
2021-12-16 15:44:39 +0900

    Add files via upload

---
+++ js/문제/q026/q026.js
@@ -0,0 +1,13 @@
+function w(Name, Age, height, weight){  
+    alert("이름: " + Name + "\n나이: " + Age + "살\n키: " + height + "cm\n몸무게: " + weight + "kg");
+}
+
+w("김유라", 21, 163, 50);
+
+w("문채연", 21, 166.7, 51);
+
+w("박예환", "?", 156, 46);
+
+w("김재형", 21, 170, 53);
+
+
