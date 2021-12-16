691ffee46dd279245711a2711270eb896492d899
zm445 <77592061+zm445@users.noreply.github.com>
2021-12-16 18:25:04 +0900

    Add files via upload

--- js/문제/q029/q029.js
+++ js/문제/q029/q029.js
@@ -1,16 +1,24 @@
-let com = Math.floor(Math.random() * 3);
-let player = prompt("가위(0) 바위(1) 보(2) 입력: ");
+while(1){
+    let player = prompt("[EXIT:-1] 가위(0) 바위(1) 보(2) 입력:");
+    let com = Math.floor(Math.random() * 3);
 
-if(player == com){
-    dw("가위(0) 바위(1) 보(2) draw <br>player: " + player + "<br>com: " + com);
-}
-else if(player == 0 && com == 1){
-    dw("가위(0) 바위(1) 보(2) computer win! <br>player: " + player + "<br>com: " + com );
-} else if(player == 1 && com == 2){
-    dw("가위(0) 바위(1) 보(2) computer win! <br>player: " + player + "<br>com: " + com );
-} else if(player == 2 && com == 0){
-    dw("가위(0) 바위(1) 보(2) computer win! <br>player: " + player + "<br>com: " + com );
-} else {
-    dw("가위(0) 바위(1) 보(2) player win! <br>player: " + player + "<br>com: " + com);
+    if(player == com){
+        alert("가위(0) 바위(1) 보(2) \ndraw \nplayer: " + player + "\ncom: " + com);
+    }
+    else if(player == 0 && com == 1){
+        alert("가위(0) 바위(1) 보(2) \ncomputer win! \nplayer: " + player + "\ncom: " + com);
+    } else if(player == 1 && com == 2){
+        alert("가위(0) 바위(1) 보(2) \ncomputer win! \nplayer: " + player + "\ncom: " + com);
+    } else if(player == 2 && com == 0){
+        alert("가위(0) 바위(1) 보(2) \ncomputer win! \nplayer: " + player + "\ncom: " + com);
+    } else if(player == -1){
+        alert("게임 종료");
+        break;
+    } else if( (player < 0 || 2 < player) && player != -1) {
+        alert("(0~2)사이의 숫자를 입력하세요.");
+    } 
+    else {
+        alert("가위(0) 바위(1) 보(2) \nplayer win! \nplayer: " + player + "\ncom: " + com);
+    }
 }
 
