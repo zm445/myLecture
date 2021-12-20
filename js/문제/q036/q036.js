3f12f2b86c95507e73e4fb98ec141fd91a1fb8cf
zm445 <77592061+zm445@users.noreply.github.com>
2021-12-20 15:31:27 +0900

    Add files via upload

--- js/문제/q036/q036.js
+++ js/문제/q036/q036.js
@@ -1,9 +1,9 @@
-var num = [];
+var num = new Array();
 
-for(var i=0; i<num.length; i++){
+for(var i=0; i<7; i++){
     num[i] = Math.floor(Math.random() * 45+1);
 }
 
-for(var i=0; i<num.length; i++){
+for(var i=0; i<7; i++){
     dw(num[i] + " ");
 }
