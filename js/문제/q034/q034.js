0b2c104a457f297891672bb72e84d18fd6f26d81
zm445 <77592061+zm445@users.noreply.github.com>
2021-12-20 15:10:48 +0900

    Add files via upload

---
+++ js/문제/q034/q034.js
@@ -0,0 +1,20 @@
+var animal = ["개", "고양이", "너굴맨", "토끼"];
+
+// 방법1
+dw(animal[0]);
+br();
+dw(animal[1]);
+br();
+dw(animal[2]);
+br();
+dw(animal[3]);
+
+br(); br();
+
+// 방법2
+
+for(var i=0; i<4; i++){
+    dw(animal[i]);
+    br();
+}
+
